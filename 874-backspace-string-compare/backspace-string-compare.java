42cc7c49d7848aabda98341033dae5e7f9c4d18f
Kotha Chaitanya <kothachaitanya04@gmail.com>
2026-09-16 09:17:02 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (78.84%) - LeetSync

---
+++ 874-backspace-string-compare/backspace-string-compare.java
@@ -0,0 +1,64 @@
+class Solution {
+    public boolean backspaceCompare(String s, String t) {
+       int i=s.length()-1;
+       int j=t.length()-1;
+       int s1=0;
+       int s2=0;
+       while(i>=0 || j>=0)
+       {
+        while(i>=0)
+        {
+            if(s.charAt(i)=='#')
+            {
+                s1++;
+                i--;
+            }
+            else if(s1>0)
+            {
+                s1--;
+                i--;
+            }
+            else
+            {
+                break;
+            }
+        }
+        while(j>=0)
+        {
+            if(t.charAt(j)=='#')
+            {
+                s2++;
+                j--;
+            }
+            else if(s2>0)
+            {
+                s2--;
+                j--;
+            }
+            else
+            {
+                break;
+            }
+        }
+         if(i>=0 && j<0)
+         {
+            return false;
+         }
+         if(j>=0 && i<0)
+         {
+            return false;
+         }
+         if(i>=0 && j>=0)
+         {
+            if(s.charAt(i)!=t.charAt(j))
+            {
+                return false;
+            }
+         }
+        i--;
+        j--;
+       }
+       return true;
+
+    }
+}
