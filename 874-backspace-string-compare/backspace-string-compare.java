bbd6009cfb7ae1c3bea2a0c4ee0839996ac1b087
Kotha Chaitanya <kothachaitanya04@gmail.com>
2026-09-16 09:41:57 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (99.37%) - LeetSync

--- 874-backspace-string-compare/backspace-string-compare.java
+++ 874-backspace-string-compare/backspace-string-compare.java
@@ -1,64 +1,63 @@
 class Solution {
     public boolean backspaceCompare(String s, String t) {
-       int i=s.length()-1;
-       int j=t.length()-1;
-       int s1=0;
-       int s2=0;
-       while(i>=0 || j>=0)
-       {
-        while(i>=0)
+        int i=s.length()-1;
+        int j=t.length()-1;
+        int s1=0;
+        int s2=0;
+        while(i>=0 || j>=0)
         {
-            if(s.charAt(i)=='#')
+            while(i>=0)
             {
+                if(s.charAt(i)=='#')
+                {
                 s1++;
                 i--;
-            }
-            else if(s1>0)
-            {
+                 }
+               else if(s1>0)
+                {
                 s1--;
                 i--;
-            }
-            else
-            {
+
+                 }
+                 else
+                {
                 break;
+                }
             }
-        }
-        while(j>=0)
-        {
-            if(t.charAt(j)=='#')
+            while(j>=0)
             {
+                if(t.charAt(j)=='#')
+                {
                 s2++;
                 j--;
-            }
-            else if(s2>0)
-            {
+                }
+                else if(s2>0)
+                {
                 s2--;
                 j--;
-            }
-            else
-            {
+                 }
+                 else
+                 {
                 break;
+                 }
             }
-        }
-         if(i>=0 && j<0)
-         {
-            return false;
-         }
-         if(j>=0 && i<0)
-         {
-            return false;
-         }
-         if(i>=0 && j>=0)
-         {
-            if(s.charAt(i)!=t.charAt(j))
+            if(i>=0 && j>=0 && s.charAt(i)!=t.charAt(j))
             {
                 return false;
             }
-         }
-        i--;
-        j--;
-       }
-       return true;
+            if(i>=0 && j<0)
+            {
+                return false;
+            }
+            if(j>=0 && i<0)
+            {
+                return false;
+            }
+           
+            i--;
+            j--;
+        }
+        return true;
 
     }
 }
